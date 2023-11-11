628dd9f4dda992e2ad26978eef93cc115bdddb6d
Eduardo Silva Barros <eduardosbarros.ed@gmail.com>
2023-11-11 00:51:04 -0300

    Portfolio 1.9 FINAL

--- Portfolio/src/app/components/options/options.component.ts
+++ Portfolio/src/app/components/options/options.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./options.component.scss']
 })
 export class OptionsComponent {
+   ToHome() {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    })
+   }
 
+   ToAbout() {
+    window.scroll({
+      top: 1000,
+      behavior: 'smooth'
+    })
+   }
+   
+   ToSkills() {
+    window.scroll({
+      top: 2000,
+      behavior: 'smooth'
+    })
+   }
+   
+   ToProjects() {
+    window.scroll({
+      top: 3000,
+      behavior: 'smooth'
+    })
+   }
 }
